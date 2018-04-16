d10ff5b4516a0a9c0d1d3e7bce0dcb70bee07c87
Audielite <emailemail.com>
2018-04-16 16:49:22 -0500

    Added delete and favroite buttons and somme css styling.

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var indexRouter = require('./routes/index');
+var favorites = require('./routes/favorites');
 
 var app = express();
 
@@ -24,8 +29,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var favorites_db_url = process.env.MONGO_URL;
+
+var store = new MongoDBStore( { uri: favorites_db_url, collection: 'sessions'}, function(err){
+  if (err) {
+    console.error('Error: can\'t connect to MongoDB to store favorites' + err);
+  }
+})
+
 app.use('/', index);
 app.use('/users', users);
+
+app.use(session({
+    secret: 'top secret!',
+    saveUninitialized: true,
+    resave: true,
+    store: store
+}));
+
+app.use('/', indexRouter);
+app.use('/favorites', favorites);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
